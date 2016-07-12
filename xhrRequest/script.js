00f49d47f4f58488058f970a02e7b860e91e5ef8
Paul Borawski <paulborawski14@gmail.com>
2016-07-12 08:04:17 -0700

    write data.txt to the browser

--- xhrRequest/script.js
+++ xhrRequest/script.js
@@ -2,5 +2,6 @@ var request = new XMLHttpRequest()
 request.open('GET', 'data.txt', false)
 request.send()
 if (request.status===200){
-console.log(request)
+  console.log(request)
+  document.write(request.response)
 }
